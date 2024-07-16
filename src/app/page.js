26bba775e0fe03e5985b92ee96ae6ebb041cba27
ManI <letz0703@gmail.com>
2024-07-17 02:35:19 +0900

    button

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,20 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
     <div className="">
       <div className="backgroundDesktop2"></div>
       <div className="backgroundMobile"></div>
       <div className="linkContainer">
-        <a
-          href="https://bit.ly/3VNITDE"
-          className="linkButton" // 클래스 이름이 linkBuggon에서 linkButton으로 오타 수정
-          rel="noopener noreferrer" // 보안 관련 속성 추가
-          target="_blank" // 새 탭에서 링크 열기
-        >
-          CANMART BUSAN
-        </a>
+      <a
+            href="https://toothsome-mouth-964.notion.site/ded72d92fd7d4d9aadbe986bb4db6b8d"
+            className="linkButton" // 클래스 이름이 linkBuggon에서 linkButton으로 오타 수정
+            rel="noopener noreferrer" // 보안 관련 속성 추가
+            target="_blank" // 새 탭에서 링크 열기
+          >
+            CANMART BUSAN
+          </a>
+
       </div>
     </div>
   </div>
