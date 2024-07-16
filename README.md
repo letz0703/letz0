1d09e8f2c1ef05f0e4fcd90e9d0856f337b6b988
ManI <letz0703@gmail.com>
2024-07-17 04:17:53 +0900

    2024.07.17 수 DONE

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-
+2024.07.17 수 /// https://arc.net/e/4003E4B5-8225-408C-8575-9D51C7D7CCB0
 [v] fix netlify error 2024.07.17 수
-https://arc.net/e/BC3BF0BD-ED25-4D93-9DCC-2734B09001ED
+netlify.toml → https://arc.net/e/BC3BF0BD-ED25-4D93-9DCC-2734B09001ED
+	```toml
+	[build]
+  	command = "next build"
+  	publish = ".next"
+
+	[[plugins]]
+  	package = "@netlify/plugin-nextjs"
+
+	[build.environment]
+  	NETLIFY_NEXT_PLUGIN_SKIP = "true"
+	```
+[v] css container errer : https://arc.net/e/5C1339CB-FBCC-47E2-801C-D4FA30413727
--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,18 @@ import "./globals.css";
 export default function Home() {
   return (
     <div className="container">
-    <div className="">
       <div className="backgroundDesktop2"></div>
       <div className="backgroundMobile"></div>
       <div className="linkContainer">
-      <a
-            href="https://toothsome-mouth-964.notion.site/ded72d92fd7d4d9aadbe986bb4db6b8d"
-            className="linkButton" // 클래스 이름이 linkBuggon에서 linkButton으로 오타 수정
-            rel="noopener noreferrer" // 보안 관련 속성 추가
-            target="_blank" // 새 탭에서 링크 열기
-          >
-            CANMART BUSAN
-          </a>
-
+        <a
+          href="https://toothsome-mouth-964.notion.site/ded72d92fd7d4d9aadbe986bb4db6b8d"
+          className="linkButton"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          CANMART BUSAN
+        </a>
       </div>
     </div>
-  </div>
   );
 }
