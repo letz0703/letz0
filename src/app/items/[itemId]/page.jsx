0b4fca5f96e32c4d178019b27704aef4eb44a241
ManI <letz0703@gmail.com>
2024-09-01 09:17:38 +0900

    query remove

--- src/app/items/[itemId]/page.jsx
+++ src/app/items/[itemId]/page.jsx
@@ -1,49 +1,49 @@
-"use client"; // Next.js 13+에서 클라이언트 컴포넌트로 선언
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+'use client' // Next.js 13+에서 클라이언트 컴포넌트로 선언
+import { useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-export default function ItemDetail() {
-  const { itemId } = useParams(); // URL에서 itemId를 가져옵니다.
-  const [item, setItem] = useState(null);
+export default function ItemDetail ({ params }) {
+  const { itemId } = useParams() // URL에서 itemId를 가져옵니다.
+  const [item, setItem] = useState(null)
 
   useEffect(() => {
     // 로컬 스토리지에서 데이터를 가져옵니다.
-    const localJapitemsString = localStorage.getItem('japitems');
+    const localJapitemsString = localStorage.getItem('japitems')
     if (!localJapitemsString) {
-      console.log('No items found in local storage.');
-      return;
+      console.log('No items found in local storage.')
+      return
     }
-    let localJapitems;
+    let localJapitems
     try {
-      localJapitems = JSON.parse(localJapitemsString);
+      localJapitems = JSON.parse(localJapitemsString)
     } catch (error) {
-      console.error('Error parsing local storage data:', error);
-      return;
+      console.error('Error parsing local storage data:', error)
+      return
     }
 
     // itemId와 일치하는 데이터를 찾습니다 (문자열 비교)
-    const foundItem = localJapitems.find((item) => String(item.id) === itemId);
+    const foundItem = localJapitems.find(item => String(item.id) === itemId)
 
     if (foundItem) {
-      setItem(foundItem);
+      setItem(foundItem)
     } else {
-      console.log(`Item with id ${itemId} not found in local storage.`);
+      console.log(`Item with id ${itemId} not found in local storage.`)
     }
-  }, [itemId]);
+  }, [itemId])
 
   if (!item) {
-    return <p>Loading...</p>;
+    return <p>Loading...</p>
   }
 
   return (
     <>
-      <h1>Item ID: {itemId}</h1>
+      <h1>Item ID: {item.id}</h1>
       <p>Name: {item.name}</p>
       <p>Price: {item.price}</p>
       <p>English Name: {item.enName}</p>
       <p>Description: {item.description}</p>
     </>
-  );
+  )
 }
 
 //"use client";
--- src/app/items/page.jsx
+++ src/app/items/page.jsx
@@ -1,78 +1,77 @@
-"use client"
+'use client'
 
-import { useEffect, useState, useCallback, useRef } from "react";
-import { database } from "@/api/firebase";
-import { get, ref } from "firebase/database";
-import styles from './Page.module.css'; // CSS 파일을 임포트
-import Link from "next/link";
+import { useEffect, useState, useCallback, useRef } from 'react'
+import { database } from '@/api/firebase'
+import { get, ref } from 'firebase/database'
+import styles from './Page.module.css' // CSS 파일을 임포트
+import Link from 'next/link'
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [japitems, setJapitems] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filteredJapitems, setFilteredJapitems] = useState([]);
-  const searchInputRef = useRef(null); // 검색 입력창의 ref
+export default function Page () {
+  const [isLoading, setIsLoading] = useState(true)
+  const [japitems, setJapitems] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [filteredJapitems, setFilteredJapitems] = useState([])
+  const searchInputRef = useRef(null) // 검색 입력창의 ref
 
-  async function getRemoteJapitems() {
+  async function getRemoteJapitems () {
     await get(ref(database, 'japitems'))
       .then(snapshot => {
         if (snapshot.exists()) {
-          const remoteJapitems = Object.values(snapshot.val());
-          setJapitems(remoteJapitems);
-          localStorage.setItem("japitems", JSON.stringify(remoteJapitems));
+          const remoteJapitems = Object.values(snapshot.val())
+          setJapitems(remoteJapitems)
+          localStorage.setItem('japitems', JSON.stringify(remoteJapitems))
         }
       })
-      .finally(() => setIsLoading(false));
+      .finally(() => setIsLoading(false))
   }
 
   useEffect(() => {
-    const localJapitems = localStorage.getItem("japitems");
+    const localJapitems = localStorage.getItem('japitems')
     if (localJapitems) {
-      setJapitems(JSON.parse(localJapitems));
-      setIsLoading(false);
+      setJapitems(JSON.parse(localJapitems))
+      setIsLoading(false)
     } else {
-      setIsLoading(false);
+      setIsLoading(false)
     }
-    getRemoteJapitems();
-  }, []);
+    getRemoteJapitems()
+  }, [])
 
   useEffect(() => {
     if (searchInputRef.current) {
-      searchInputRef.current.focus(); // 페이지 로딩 시 검색 입력창에 포커스
+      searchInputRef.current.focus() // 페이지 로딩 시 검색 입력창에 포커스
     }
-  }, []);
+  }, [])
 
   const debounce = (func, delay) => {
-    let debounceTimer;
-    return function(...args) {
-      const context = this;
-      clearTimeout(debounceTimer);
-      debounceTimer = setTimeout(() => func.apply(context, args), delay);
-    };
-  };
+    let debounceTimer
+    return function (...args) {
+      const context = this
+      clearTimeout(debounceTimer)
+      debounceTimer = setTimeout(() => func.apply(context, args), delay)
+    }
+  }
 
   const handleSearch = useCallback(
-    debounce((searchTerm) => {
+    debounce(searchTerm => {
       setFilteredJapitems(
         japitems.filter(item => {
-          const term = searchTerm.toLowerCase();
+          const term = searchTerm.toLowerCase()
           return (
             item.name.toLowerCase().includes(term) ||
             item.description.toLowerCase().includes(term) ||
             item.enName.toLowerCase().includes(term) ||
             item.price.toString().includes(term)
-          );
+          )
         })
-      );
+      )
     }, 300),
     [japitems]
-  );
+  )
 
   useEffect(() => {
-    handleSearch(searchTerm);
-  }, [searchTerm, handleSearch]);
-  console.log(filteredJapitems);
-
+    handleSearch(searchTerm)
+  }, [searchTerm, handleSearch])
+  console.log(filteredJapitems)
 
   return (
     <div className={styles.pageContainer}>
@@ -80,15 +79,21 @@ export default function Page() {
         <h2>Loading...</h2>
       ) : (
         <div>
-          <div className={`${styles.searchContainer} mt-10 mb-10`}> {/* Tailwind CSS로 상하 마진 추가 */}
+          <div className={`${styles.searchContainer} mt-10 mb-10`}>
+            {' '}
+            {/* Tailwind CSS로 상하 마진 추가 */}
             <span className={styles.searchIcon}>
-              <img src="/images/search-48.png" alt="Search Icon" className={styles.searchIconImage} />
+              <img
+                src='/images/search-48.png'
+                alt='Search Icon'
+                className={styles.searchIconImage}
+              />
             </span>
             <input
-              type="text"
-              placeholder="Price Info"
+              type='text'
+              placeholder='Price Info'
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={e => setSearchTerm(e.target.value)}
               className={`${styles.searchInput} mt-10 mb-10 pl-10`} // Tailwind CSS로 상하 마진 추가 및 좌측 패딩 추가
               ref={searchInputRef} // ref 연결
             />
@@ -98,14 +103,18 @@ export default function Page() {
               <div key={index} className={styles.itemCard}>
                 <Link
                   href={{
-                    pathname:`/items/${item.id}`
-                    ,
-                    query: { itemId:item.id, name: item.name, price:item.price, enName:item.enName, description:item.description}
+                    pathname: `/items/${item.id}`
+                    //query: { itemId:item.id, name: item.name, price:item.price, enName:item.enName, description:item.description}
                   }}
-                //href={item.homeUrl}
-                //target="_blank" rel="noopener noreferrer"
-                style={{ textDecoration: 'none', color: 'inherit' }}>
-                  <img src={`/images/${item.imgs}`} alt={item.name} className={styles.itemImage} />
+                  //href={item.homeUrl}
+                  //target="_blank" rel="noopener noreferrer"
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                  <img
+                    src={`/images/${item.imgs}`}
+                    alt={item.name}
+                    className={styles.itemImage}
+                  />
                   <p>{item.name}</p>
                   <p>{item.price}</p>
                   <p>{item.enName}</p>
@@ -117,5 +126,5 @@ export default function Page() {
         </div>
       )}
     </div>
-  );
+  )
 }
