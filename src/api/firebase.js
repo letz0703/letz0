d67eba46dc69897b88fc12a0fdb8aa7666cc4445
ManI <letz0703@gmail.com>
2024-09-17 02:40:08 +0900

    remove is loading

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -22,7 +22,23 @@ export const database = getDatabase(app);
 export const db = getFirestore(app);
 
 // Function to get japitems from Firebase Realtime Database
-export const getJapItemsFromFirebase = async () => {
+//export const getJapItemsFromFirebase = async () => {
+//  try {
+//    const japitemsRef = ref(database, 'japitems'); // 'japitems' 경로에서 데이터를 읽음
+//    const snapshot = await get(japitemsRef);
+//    if (snapshot.exists()) {
+//      return snapshot.val(); // 데이터를 반환
+//    } else {
+//      return []; // 데이터가 없으면 빈 배열 반환
+//    }
+//  } catch (error) {
+//    console.error('Error fetching japitems from Firebase:', error);
+//    return []; // 에러 발생 시 빈 배열 반환
+//  }
+//};
+
+export default async function getJapitemsFromFirebase(){
+  wait(2000)
   try {
     const japitemsRef = ref(database, 'japitems'); // 'japitems' 경로에서 데이터를 읽음
     const snapshot = await get(japitemsRef);
@@ -35,4 +51,10 @@ export const getJapItemsFromFirebase = async () => {
     console.error('Error fetching japitems from Firebase:', error);
     return []; // 에러 발생 시 빈 배열 반환
   }
-};
+}
+
+function wait(duration) {
+  return new Promise(resolve => {
+    setTimeout(resolve, duration)
+  })
+}
