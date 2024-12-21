2d48c9d417596a61f29d5ce248a8e874521c75be
ManI <letz0703@gmail.com>
2024-12-22 01:26:42 +0900

    before font

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,15 @@ import "./globals.css";
 export default function Home() {
   return (
     <div className="container">
-      <div className="backgroundDesktop2"></div>
-      <div className="backgroundMobile"></div>
+      <div className="title">부산 깡통시장 가격정보</div>
       <div className="linkContainer">
         <a
-          //href="https://toothsome-mouth-964.notion.site/ded72d92fd7d4d9aadbe986bb4db6b8d"
-        href="/items"
+          href="/items"
           className="linkButton"
           rel="noopener noreferrer"
           target="_blank"
         >
-          BUYing Agent - Canmart BUSAN, KOREA
+          PRICE INFO
         </a>
       </div>
     </div>
