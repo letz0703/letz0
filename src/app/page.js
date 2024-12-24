2fba7c2351074fda875cad9cc85dab11d95030af
ManI <letz0703@gmail.com>
2024-12-24 23:03:23 +0900

    2024.12.24 화 buy me fix

--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,14 @@ export default function Home() {
       {/*<div className={inter.className}>부산 깡통시장 가격정보</div>*/}
       <div className={gowoon.className} style={{fontSize:"2em"}}>부산 깡통시장 가격정보</div>
       <div className={inter.className} style={{fontSize:"1.4em", color:"gray"}}>price info - CANMART, KOREA</div>
+      <br/>
       <div className="linkContainer">
         <a
           href="/items"
           className="linkButton"
           rel="noopener noreferrer"
           target="_blank"
-        >
-         최저가 검색
+        >🔎 PRICE INFO
         </a>
       </div>
     </div>
