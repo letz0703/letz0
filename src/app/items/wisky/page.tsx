1fc4b616ebc45190174fb6c2ad6b70924a076090
ManI <letz0703@gmail.com>
2025-02-03 20:21:50 +0900

    2025.02.03 월

--- src/app/items/wisky/page.tsx
+++ src/app/items/wisky/page.tsx
@@ -3,17 +3,22 @@ import "./style.css"; // 이미지 스타일 불러오기
 import Link from "next/link"
 
 export default function Page() {
+  const imageNames = ["ballan.jpg", "japanwisky.jpg"]
   return (
     <div className="divOverlay"
     >
       {/* 4K 이미지 */}
+      {imageNames.map((name, index) =>(
+
+
       <Image
-        src="/images/ballan.jpg" // public 폴더 내 이미지 경로
+        src={`/images/${name}`} // public 폴더 내 이미지 경로
         alt="cover page"
 				width={3840}
 				height ={2160}
         className="image4K"
       />
+    ))}
 
       {/* 오버레이 텍스트 */}
       {/*<div className="overlayText"
@@ -23,9 +28,9 @@ export default function Page() {
 			<Link
       href="https://www.youtube.com/playlist?list=PLxJmwr-75ybaA0OypwGjCQSLKbyXHK5zg"
       target="_blank"
-      className="px-4 py-2 bg-yellow-400 text-black rounded-lg shadow-md hover:bg-yellow-500 transition"
+      className="fixed bottom-4 left-1/2 transform -translate-x-1/2 px-6 py-3 bg-yellow-400 text-black rounded-lg shadow-md hover:bg-yellow-500 transition"
     >
-      깡통시장 최신가격 - YOUTUBE
+      가격문의: 051.246.0909
     </Link>
     </div>
   );
